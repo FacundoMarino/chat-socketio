d83ab90254d76906d67951a572f089f62aaff73a
Facundo Marino <38021147+FacundoMarino@users.noreply.github.com>
2023-01-22 23:02:15 -0300

    fixed bugs

--- src/server.js
+++ src/server.js
@@ -2,36 +2,31 @@ import express from 'express';
 import morgan from 'morgan';
 import { Server as SocketServer } from 'socket.io';
 import http from 'http';
-import cors from 'cors';
 import { dirname, join } from 'path';
 import { fileURLToPath } from 'url';
 
 import { PORT } from './utils/config.js';
-import { url } from 'inspector';
+import cors from 'cors';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
-const __dirname = dirname(fileURLToPath(import.meta.url));
 const server = http.createServer(app);
-const io = new SocketServer(server, {
-	cors: {
-		origin: '*',
-	},
-});
+const io = new SocketServer(server, {});
 
 io.on('connection', (socket) => {
 	console.log(socket.id);
 	socket.on('message', (message) => {
 		socket.broadcast.emit('message', {
 			body: message,
-			from: socket.id,
+			from: socket.id.slice(8),
 		});
 	});
 });
 
 app.use(cors());
 app.use(morgan('dev'));
-
-app.use(express.static(join(__dirname, '../../client/build')));
+app.use(express.static(join(__dirname, '../client/build')));
 
 server.listen(PORT);
 console.log('servidor up');
